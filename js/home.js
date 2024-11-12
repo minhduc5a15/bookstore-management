0e78d0f93364fd2e9f9a9c3bb0b9ec396113e249
VuzLe <tle63917@gmail.com>
2024-11-12 17:15:36 +0700

    Thêm slide cho newBook, bestSeller, newArrival

--- js/home.js
+++ js/home.js
@@ -7,3 +7,67 @@ document.addEventListener('DOMContentLoaded', async () => {
     insertHead();
     insertFooter();
 });
+
+
+const swiper = new Swiper('.swiper-new', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    speed: 800,
+    effect: 'fade', //'cube', 'coverflow', 'flip', 'fade'
+    fadeEffect: {
+        crossFade: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+});
+
+
+const featuredSlider = new Swiper('.featured-slider', {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 20,
+    speed: 800,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+});
+
+
+const arrivalSlider = new Swiper('.arrivals-slider', {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 20,
+    speed: 800,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+});
